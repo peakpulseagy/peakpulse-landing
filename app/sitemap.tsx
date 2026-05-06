647f09a1c1067efc3f6abda70007a148c48ae7a1
peakpulseagy <fdr.peakpulse@gmail.com>
2026-05-07 04:04:07 +0800

    Fix sitemap to use production domain

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const ROOT_URL = "https://peak-pulse.vercel.app";
+  const ROOT_URL = "https://peakpulseagy.com";
   return [
     
     {
