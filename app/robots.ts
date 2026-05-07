06d65930224491d843cf4004e8ea24432599c77b
peakpulseagy <fdr.peakpulse@gmail.com>
2026-05-07 20:06:29 +0800

    Fix robots.txt sitemap URL to production domain

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://peak-pulse.vercel.app/sitemap.xml",
+    sitemap: "https://peakpulseagy.com/sitemap.xml",
   };
 }
